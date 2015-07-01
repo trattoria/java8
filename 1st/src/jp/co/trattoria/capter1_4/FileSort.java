78e3b8a8c1e1b274d4c27ee75162b0496f7f3526
hoge <hoge@hoge>
2015-07-02 00:12:33 +0900

    First commit.

---
+++ 1st/src/jp/co/trattoria/capter1_4/FileSort.java
@@ -0,0 +1,46 @@
+package jp.co.trattoria.capter1_4;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Fileクラスの配列を受け取って、ディレクトリ優先で名前をソートする。
+ */
+public class FileSort {
+
+	/**
+	 * ディレクトリ、ファイル混在のFileオブジェクト配列を、ディレクトリ優先で、名前の昇順でソートする
+	 * @param fileList 未ソートのFileオブジェクト配列
+	 * @return 文字列でソートしたFileオブジェクト配列
+	 */
+	File[] getFileListSort(final File[] fileList){
+
+		List<File> directorys = new ArrayList<File>();
+		List<File> files = new ArrayList<File>();
+
+		for(File f: fileList){
+
+			if(f.isDirectory()){
+				directorys.add(f);
+
+			}else{
+				files.add(f);
+			}
+		}
+
+		directorys.sort((File a, File b)->{
+			return a.compareTo(b);
+		});
+
+		files.sort((File a, File b)->{
+			return a.compareTo(b);
+		});
+
+		List<File> result = new ArrayList<File>();
+		result.addAll(directorys);
+		result.addAll(files);
+
+		return result.toArray(new File[0]);
+	}
+}
---
+++ 1st/test/jp/co/trattoria/capter1_4/FileSortTest.java
@@ -0,0 +1,23 @@
+package jp.co.trattoria.capter1_4;
+
+import java.io.File;
+
+import org.junit.Test;
+
+public class FileSortTest {
+
+	@Test
+	public void testGetFileListSort() {
+
+
+		File[] fileList = new File("/").listFiles();
+
+
+		File[] sortedFiles = new FileSort().getFileListSort(fileList);
+
+		for(File f: sortedFiles){
+			System.out.println(f);
+		}
+	}
+
+}
